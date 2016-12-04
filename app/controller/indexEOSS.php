c06754d52c3088cc6b5ef857a60df12e622269c8
Juraj Čarnogurský <juraj.carnogursky@student.tuke.sk>
2016-12-04 23:16:31 +0100

    m

--- app/controller/indexEOSS.php
+++ app/controller/indexEOSS.php
@@ -2,143 +2,56 @@
 
 use EOSS\EOSS;
 
+
+/**
+ * Startup class.
+ * Class indexEOSS
+ */
 class indexEOSS extends EOSS
 {
-    public $number = NULL;
-    public $operator = NULL;
-    public $newNumber = true;
-    public $enterPressed = false;
+    public $counter = 1;
 
+    /**
+     * Load is called when the EOSS is initialized.
+     * It should contain setFile to set the view, but
+     * before that it should contain parameters sent to
+     * the view. Forms should be created before setFile also
+     * and they should be passed as a parameter to the view.
+     */
     public function load()
     {
-        $this->csi->setFile("indexView.html");
+        $this->csi->params->title = "Welcome To EOSS | EOSS2";
+        $this->csi->setFile("indexView.php");
     }
 
+    /**
+     * Bind is called when the CSI(Client Side Interface)
+     * is generated. It should contain all of the basic
+     * bindings to the events.
+     */
     public function bind()
     {
-        $this->csi->b->onclick[] = "writeToDisplay";
-        $this->csi->bc->onclick[] = "clearAll";
-        $this->csi->bce->onclick[] = "clearLast";
-        $this->csi->o->onclick[] = "onOperator";
-        $this->csi->result->onclick[] = "evaluate";
-        $this->csi->calc->onkeydown[] = "keyDown";
-        $this->csi->calc->onkeypressed[] = "keyPressed";
+
+        $this->csi->txtSource->onkeypress[] = "rewrite";
+        $this->csi->txtTodo->onenterpressed[] = "addTodo";
+        $this->csi->buttons->onclick[] = "showNumber";
     }
 
-    public function writeToDisplay($sender,$anumber=-1)
-    {
-        if($this->WasEnterPressed())
-            return;
-        if($this->newNumber || $this->csi->display->html == "0")
-        {
-            $this->csi->display->html = $anumber != -1 ? strval($anumber) : strval($sender->value);
-            $this->newNumber = false;
-        }
-        else
-        {
-            $this->csi->display->html .=  $anumber != -1 ? strval($anumber) : strval($sender->value);
-        }
+    public function rewrite($sender, $keyCode) {
+        $this->csi->lblCopy->html = "<b>" . $this->csi->txtSource->value . "</b>";
     }
 
-    public function clearAll()
-    {
-        if($this->WasEnterPressed())
-            return;
-        $this->csi->display->html = '0';
-        $this->newNumber = true;
+    public function addTodo($sender) {
+        $this->csi->lblTodos->html .= "<div><b>" . $this->counter . ".: </b>" . $this->csi->txtTodo->value . "</div>";
+        $this->csi->txtTodo->value = "";
+        $this->counter++;
     }
 
-    public function clearLast()
-    {
-        if($this->WasEnterPressed())
-            return;
-        if(strlen($this->csi->display->html) == 1 || ($this->csi->display->html < 0 && strlen($this->csi->display->html)==2))
-            $this->clearAll();
-        else
-            $this->csi->display->html = substr($this->csi->display->html,0,-1);
+    public function showNumber($sender) {
+        $this->csi->lblButtons->html = $sender->value;
+        $this->flashMessage("You've successfully clicked on " . $sender->value . " button.", "success");
+        $sender->value += 1;
     }
 
-    public function onOperator($sender,$op=NULL)
-    {
-        if($this->WasEnterPressed())
-            return;
-        $op = $op? : $sender->value;
-        switch($op)
-        {
-            case '+/-':
-                $this->csi->display->html = -$this->csi->display->html;
-                break;
-            default:
-                if($this->number!=NULL)
-                    $this->evaluate();
-                $this->number = $this->csi->display->html;
-                $this->operator = $op;
-                $this->newNumber = true;
-        }
-    }
 
-    public function evaluate()
-    {
-        if($this->WasEnterPressed())
-            return;
-        switch($this->operator)
-        {
-            case '+':
-                $this->csi->display->html += $this->number;
-                break;
-            case '-':
-                $this->csi->display->html = $this->number - $this->csi->display->html;
-                break;
-            case '*':
-                $this->csi->display->html *= $this->number;
-                break;
-            case '/':
-                $this->csi->display->html = $this->number / $this->csi->display->html;
-                break;
-        }
-        $this->newNumber = true;
-        $this->number = NULL;
-    }
-
-    public function keyPressed($sender, $charCode)
-    {
-        if($charCode>47 && $charCode<58)
-            $this->writeToDisplay(NULL,$charCode - 48);
-        else
-            switch($charCode)
-            {
-                case 46:
-                    $this->writeToDisplay(NULL,"."); //chr($charCode));
-                case 42:
-                case 43:
-                case 45:
-                case 47:
-                    $this->onOperator(NULL,chr($charCode));
-                    break;
-                case 13:
-                    $this->evaluate();
-                    $this->enterPressed = true;
-                    break;
-            }
-    }
-
-    private function WasEnterPressed()
-    {
-        $result = $this->enterPressed;
-        $this->enterPressed = false;
-        return $result;
-    }
-
-    public function keyDown($sender, $keyCode)
-    {
-        switch($keyCode)
-        {
-            case 8:
-                $this->clearLast();
-                break;
-            case 27:
-                $this->clearAll();
-                break;
-        }
-    }
 }
