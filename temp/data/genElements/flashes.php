a831623d200423048c99a90952034f847b0349c3
Juraj Čarnogurský <juraj.carnogursky@student.tuke.sk>
2016-12-08 18:32:24 +0100

    untracked files

---
+++ temp/data/genElements/flashes.php
@@ -0,0 +1,82 @@
+<?php
+class flashes { 
+
+	/**
+	 * @var string
+	 */
+	public $id;
+
+	/**
+	 * @var string
+	 */
+	public $data_ignore;
+
+	/**
+	 * @var string
+	 */
+	public $type;
+
+	/**
+	 * @var string
+	 */
+	public $html;
+
+	/**
+	 * @var array
+	 */
+	public $onclick = array();
+
+	/**
+	 * @var array
+	 */
+	public $onhover = array();
+
+	/**
+	 * @var array
+	 */
+	public $onchange = array();
+
+	/**
+	 * @var array
+	 */
+	public $onfocus = array();
+
+	/**
+	 * @var array
+	 */
+	public $onfocusin = array();
+
+	/**
+	 * @var array
+	 */
+	public $onfocusout = array();
+
+	/**
+	 * @var array
+	 */
+	public $onload = array();
+
+	/**
+	 * @var array
+	 */
+	public $onmousedown = array();
+
+	/**
+	 * @var array
+	 */
+	public $onkeypress = array();
+
+	/**
+	 * @var array
+	 */
+	public $onenterpressed = array();
+
+
+	public function __construct() { 
+		$this->id="flashes";
+		$this->data_ignore="";
+		$this->type="div";
+		$this->html="";
+	}
+
+}
