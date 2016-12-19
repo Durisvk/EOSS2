fa820ab38bc7aa1b34e792519f49bb5d257d6154
Juraj Čarnogurský <juraj.carnogursky@student.tuke.sk>
2016-12-19 20:33:33 +0100

    serialize deserialize functionallity deleted

--- libs/Utils/EOSSHelper.php
+++ libs/Utils/EOSSHelper.php
@@ -6,31 +6,63 @@ namespace Utils;
 
 use EOSS\EOSS;
 
-/**
- * Simple EOSS state saving and restoring class.
- * Class EOSSHelper
- * @package Utils
- */
 class EOSSHelper
 {
 
-    /**
-     * Saves the EOSS state into sessions.
-     * @param EOSS $eoss
-     * @param string $name
-     */
-    public static function storeClassVariables(EOSS $eoss,$name) {
+    public static function storeClassVariables($eoss,$name) {
         Session::getInstance()->set($name, "");
-        Session::getInstance()->set($name,serialize($eoss));
+        unset($eoss->csi->eoss);
+        foreach($eoss as $key => $value) {
+            if($value instanceof \Binding\IBindableProperty) {
+                $eoss->$key = $value->get();
+            }
+        }
+        $vars = get_object_vars($eoss);
+        foreach($vars as $key => $value) {
+            if(!is_scalar($value) && $key != 'csi') {
+                $vars[$key] = 'model';
+            }
+        }
+        Session::getInstance()->set($name,json_encode($vars));
+
+    }
+    public static function restoreClassVariables(&$eoss,$name) {
+        $json=Session::getInstance()->get($name);
+        $eoss=self::jsonToEoss($eoss,JSON::decode($json));
     }
 
     /**
-     * Restores the EOSS state from the sessions.
+     * Loads the EOSS from JSON format
      * @param EOSS $eoss
-     * @param string $name
+     * @param array $json
+     * @return mixed
      */
-    public static function restoreClassVariables(EOSS &$eoss,$name) {
-        $data=Session::getInstance()->get($name);
-        $eoss = unserialize($data);
+    public static function jsonToEoss ($eoss,$json) {
+        foreach ($json as $key=>$val) {
+            if($key == "csi") {
+                foreach ($val as $elkey=>$elval) {
+                    foreach($elval as $attkey=>$attval) {
+                        if(isset($eoss->$key->$elkey->$attkey) && !Strings::startsWith($attkey, 'on')) {
+                            $eoss->$key->$elkey->$attkey = $attval;
+                        }
+                    }
+                }
+            } else {
+                if($key != "redirect") {
+                    if($eoss->$key instanceof \Binding\IBindableProperty) {
+                        $eoss->$key->set($val);
+                    } else if($eoss->$key instanceof \Binding\BindableCollection) {
+                        $eoss->$key->setValue($val);
+                    }else {
+                        if($val == "model") {
+                            continue;
+                        }
+                        $eoss->$key = $val;
+                    }
+                }
+            }
+        }
+        return $eoss;
     }
+
 }
